073dd008678ceb1e46ab8630c2a5f8c378f8237c
spinachElf <70534964+spinachElf@users.noreply.github.com>
2023-05-22 02:07:12 +0900

    Update stretch.js

--- stretch.js
+++ stretch.js
@@ -24,7 +24,7 @@ var ground;
 // Render.run(render);
 
 function setup() {
-  createCanvas(2300, 2000);
+  createCanvas(2300, 1286);
   engine = Engine.create();
   world = engine.world;
   // Engine.run(engine);
